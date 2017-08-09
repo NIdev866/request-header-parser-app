2a2a35eeb20f3ff7643676b51b1100ec91317e3c
NIdev866 <nidev866@gmail.com>
2017-08-09 22:20:34 +0100

    1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = Number(process.env.PORT||8080)
 const expressIpv4 = require("express-ipv4")
+const requestIp = require('request-ip');
 
 app.set('trust_proxy', 1)
 
@@ -11,7 +12,7 @@ app.use(expressIpv4())
 app.get("*", (req, res) => {
 
   //const ip = req.ip
-  const ip = req.ip
+  const ip = requestIp.getClientIp(req)
   const languageUnedited = req.headers["accept-language"]
   const language = languageUnedited.substring(0, languageUnedited.indexOf(","))
   const softwareUnedited = req.headers["user-agent"]
